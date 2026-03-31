8b70130e66b1027ff60e5d94ba10edfb66d91c4d
seyoung1007 <bobb7771@gmail.com>
2026-03-31 13:03:33 +0900

    STX 클론코딩공부

---
+++ modal.js
@@ -0,0 +1,19 @@
+window.addEventListener("load", function () {
+  // gotop 스크롤 이벤트
+  // 변수 선언
+  const goTop = this.document.querySelector("#gotop");
+  this.window.addEventListener("scroll", () => {
+    if (this.document.documentElement.scrollTop > 1000) {
+      goTop.style.display = "block";
+    } else {
+      goTop.style.display = "none";
+    }
+  });
+  //   gotop버튼 클릭시 위로 스크롤
+  goTop.addEventListener("click", () => {
+    this.window.scrollTo({
+      top: 0,
+    //   behavior: "smooth",
+    });
+  });
+});
